104ba9c22d86b58f483083d2ba909e92c6bd56c0
Aandeel <65628869+aandeel@users.noreply.github.com>
2024-04-03 16:02:28 +0100

    Create Version.svelte

---
+++ src/lib/reusables/Version.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import IconTick from "~icons/tabler/rosette-discount-check";
+</script>
+
+<div class="bg-green-500/25 w-fit min-w-[25%] border-l-4 border-blue-600 p-3 my-2">
+  <div class="flex text-sm space-x-2 items-center text-blue-400 mb-1">
+    <IconTick />
+    <slot />
+  </div>
+</div>
