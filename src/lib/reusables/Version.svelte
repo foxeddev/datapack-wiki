247e74b510cd4c79316e2bde4b8d74c9e0fa0da7
Aandeel <65628869+aandeel@users.noreply.github.com>
2024-04-03 16:14:20 +0100

    Update Version.svelte

--- src/lib/reusables/Version.svelte
+++ src/lib/reusables/Version.svelte
@@ -2,9 +2,9 @@
   import IconTick from "~icons/tabler/rosette-discount-check";
 </script>
 
-<div class="bg-green-500/25 w-fit min-w-[25%] border-l-4 border-blue-600 p-3 my-2">
-  <div class="flex text-sm space-x-2 items-center text-blue-400 mb-1">
-    <IconTick />
+<div class="bg-green-500/25 w-fit min-w-[25%] border-l-4 border-green-600 p-3 my-2">
+  <div class="flex text-sm space-x-2 items-center text-white-400 mb-1">
+    <IconTick /> 
     <slot />
   </div>
 </div>
