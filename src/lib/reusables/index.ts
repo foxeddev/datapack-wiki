e40b37e8d2bc82a9fe0bfd8e408317c2c110987d
Aandeel <65628869+aandeel@users.noreply.github.com>
2024-04-03 16:11:35 +0100

    Update index.ts

--- src/lib/reusables/index.ts
+++ src/lib/reusables/index.ts
@@ -2,3 +2,4 @@ export { default as Info } from "./Info.svelte";
 export { default as Tip } from "./Tip.svelte";
 export { default as Warning } from "./Warning.svelte";
 export { default as LatestPackFormat } from "./LatestPackFormat.svelte";
+export { default as Version } from "./Version.svelte";
