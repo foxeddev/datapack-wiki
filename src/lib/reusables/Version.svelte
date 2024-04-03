1eecf09367e597a7c6e1f6134fa4e7cb828c770d
Aandeel <65628869+aandeel@users.noreply.github.com>
2024-04-03 16:16:11 +0100

    Update Version.svelte

--- src/lib/reusables/Version.svelte
+++ src/lib/reusables/Version.svelte
@@ -4,7 +4,6 @@
 
 <div class="bg-green-500/25 w-fit min-w-[25%] border-l-4 border-green-600 p-3 my-2">
   <div class="flex text-sm space-x-2 items-center text-white-400 mb-1">
-    <IconTick /> 
-    <slot />
+    <IconTick /> <slot />
   </div>
 </div>
