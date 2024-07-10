efe84e359fca4ba7e4d57107e75e6a09cc57644a
Cobblestone <aidenhperry@gmail.com>
2024-07-10 13:04:18 -0400

    initial port of datapack helper method guides

--- src/lib/sidebar-pages/Guides.svelte
+++ src/lib/sidebar-pages/Guides.svelte
@@ -2,7 +2,6 @@
   // Page imports
   import SidebarPage from "$lib/sidebar/SidebarPage.svelte";
   import SidebarCategory from "$lib/sidebar/SidebarCategory.svelte";
-  import SidebarHeading from "$lib/sidebar/SidebarHeading.svelte";
 
   // Icon imports
   // https://tabler-icons.io
@@ -12,6 +11,9 @@
   import IconNew from "~icons/tabler/folder-plus";
   import IconMusic from "~icons/tabler/music";
   import IconBug from "~icons/tabler/bug";
+  import IconFileZip from "~icons/tabler/zip";
+  import IconArrow from "~icons/tabler/ArcheryArrow";
+  import IconBrackets from "~icons/tabler/code";
 </script>
 
 <!-- ADD PAGES AND CATEGORIES BELOW -->
@@ -19,7 +21,21 @@
 <SidebarPage label="Home" icon={IconHome} page="/" />
 <SidebarPage label="Getting Started" icon={IconPennant} page="/guide/getting-started" />
 <SidebarPage label="Debugging your Datapacks" icon={IconBug} page="/guide/debugging-datapacks" />
+<SidebarPage label="Installing a Datapack" icon={IconFileZip} page="/guide/installing-a-datapack" />
+<SidebarPage label="Creating a Datapack" icon={IconBrackets} page="/guide/creating-a-datapack" />
 
 <SidebarCategory name="Adding new features" icon={IconNew}>
   <SidebarPage label="Adding Jukebox Songs" icon={IconMusic} page="/guide/jukebox-songs" />
+  <SidebarPage label="Random Numbers (old)" icon={IconBrackets} page="/guide/random-number-old" />
+  <SidebarPage label="Player ID System" icon={IconBrackets} page="/guide/player-id-system" />
+  <SidebarPage label="Custom Items (old)" icon={IconBrackets} page="/guide/player-id-system" />
+  <SidebarPage label="Raycasts" icon={IconArrow} page="/guide/raycasts" />
+</SidebarCategory>
+
+<SidebarCategory name="Right Click Detection" icon={IconNew}>
+  <SidebarPage label="Summary" icon={IconPennant} page="/guide/right-click" />
+  <SidebarPage label="Carrot on a Stick" icon={IconPennant} page="/guide/right-click/coas" />
+  <SidebarPage label="Eye of Ender" icon={IconPennant} page="/guide/right-click/eoe" />
+  <SidebarPage label="Food" icon={IconPennant} page="/guide/right-click/food" />
+  <SidebarPage label="Interaction Entity" icon={IconPennant} page="/guide/right-click/interaction" />
 </SidebarCategory>
