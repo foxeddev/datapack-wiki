974db9a580fa9038ff954f21bf5ae1d79df27be7
Silabear <56885288+Silabear@users.noreply.github.com>
2024-07-10 18:20:49 +0100

    guides sidebar improvements

--- src/lib/sidebar-pages/Guides.svelte
+++ src/lib/sidebar-pages/Guides.svelte
@@ -2,6 +2,7 @@
   // Page imports
   import SidebarPage from "$lib/sidebar/SidebarPage.svelte";
   import SidebarCategory from "$lib/sidebar/SidebarCategory.svelte";
+  import SidebarHeading from "$lib/sidebar/SidebarHeading.svelte";
 
   // Icon imports
   // https://tabler-icons.io
@@ -14,28 +15,41 @@
   import IconFileZip from "~icons/tabler/zip";
   import IconArrow from "~icons/tabler/ArcheryArrow";
   import IconBrackets from "~icons/tabler/code";
+  import IconMouse from "~icons/tabler/mouse";
+  import IconCarrot from "~icons/tabler/carrot";
+  import IconEye from "~icons/tabler/eye";
+  import IconApple from "~icons/tabler/apple";
+  import IconSelection from "~icons/tabler/new-section";
+  import IconDice from "~icons/tabler/dice-5";
+  import IconID from "~icons/tabler/id-badge";
 </script>
 
 <!-- ADD PAGES AND CATEGORIES BELOW -->
 
 <SidebarPage label="Home" icon={IconHome} page="/" />
 <SidebarPage label="Getting Started" icon={IconPennant} page="/guide/getting-started" />
-<SidebarPage label="Debugging" icon={IconBug} page="/guide/debugging" />
-<SidebarPage label="Installing a Datapack" icon={IconFileZip} page="/guide/installing-a-datapack" />
-<SidebarPage label="Creating a Datapack" icon={IconBrackets} page="/guide/creating-a-datapack" />
+<SidebarPage label="Installing datapacks" icon={IconFileZip} page="/guide/installing-a-datapack" />
 
+<div class="h-2" />
+<SidebarHeading label="Advanced" />
 <SidebarCategory name="Adding new features" icon={IconNew}>
   <SidebarPage label="Adding Jukebox Songs" icon={IconMusic} page="/guide/jukebox-songs" />
-  <SidebarPage label="Random Numbers (old)" icon={IconBrackets} page="/guide/random-number-old" />
-  <SidebarPage label="Player ID System" icon={IconBrackets} page="/guide/player-id-system" />
-  <SidebarPage label="Custom Items (old)" icon={IconBrackets} page="/guide/player-id-system" />
-  <SidebarPage label="Raycasts" icon={IconArrow} page="/guide/raycasts" />
+  <SidebarPage label="Custom Items" icon={IconApple} page="/guide/custom-items" />
 </SidebarCategory>
 
-<SidebarCategory name="Right Click Detection" icon={IconNew}>
+<SidebarCategory name="Right Click Detection" icon={IconMouse}>
   <SidebarPage label="Summary" icon={IconPennant} page="/guide/right-click" />
-  <SidebarPage label="Carrot on a Stick" icon={IconPennant} page="/guide/right-click/coas" />
-  <SidebarPage label="Eye of Ender" icon={IconPennant} page="/guide/right-click/eoe" />
-  <SidebarPage label="Food" icon={IconPennant} page="/guide/right-click/food" />
-  <SidebarPage label="Interaction Entity" icon={IconPennant} page="/guide/right-click/interaction" />
+
+  <SidebarHeading label="Techniques" />
+  <SidebarPage label="Carrot on a Stick" icon={IconCarrot} page="/guide/right-click/coas" />
+  <SidebarPage label="Eye of Ender" icon={IconEye} page="/guide/right-click/eoe" />
+  <SidebarPage label="Food" icon={IconApple} page="/guide/right-click/food" />
+  <SidebarPage label="Interaction Entity" icon={IconSelection} page="/guide/right-click/interaction" />
+</SidebarCategory>
+
+<SidebarCategory name="Technical" icon={IconBrackets}>
+  <SidebarPage label="Debugging" icon={IconBug} page="/guide/debugging" />
+  <SidebarPage label="Random Numbers (old)" icon={IconDice} page="/guide/random-number-old" />
+  <SidebarPage label="Player ID System" icon={IconID} page="/guide/player-id-system" />
+  <SidebarPage label="Raycasts" icon={IconArrow} page="/guide/raycasts" />
 </SidebarCategory>
