93748be483d21d74ebf8999204ff6db8a23ebe88
Cobblestone <aidenhperry@gmail.com>
2024-09-18 18:36:14 -0400

    update deps, finally fix styling bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,16 +30,7 @@
     <Sidebar />
     <div id="content" class="py-12 w-full min-h-screen text-stone-200 bg-stone-900">
       <main
-        class="prose prose-sm prose-stone prose-invert max-w-full md:prose-base
-  prose-headings:mb-2 prose-h2:text-2xl prose-h2:mt-7 prose-headings:scroll-mt-16
-  prose-p:my-3
-  prose-blockquote:rounded-l-sm prose-blockquote:font-normal prose-blockquote:not-italic
-  prose-code:bg-stone-800 prose-code:rounded-sm prose-code:border prose-code:text-orange-300 prose-code:border-stone-700 prose-code:p-1 prose-code:font-thin prose-code:before:content-none prose-code:after:content-none prose-code:break-all
-  prose-pre:p-2 prose-code:prose-pre:p-0 prose-code:prose-pre:border-0 prose-pre:overflow-x-auto
-  prose-ol:mt-0 prose-ul:mt-0 prose-li:my-1
-  prose-h2:border-b prose-h2:pb-2 prose-h2:border-white/20
-  prose-h3:border-b prose-h3:pb-2 prose-h3:border-white/20
-  prose-th:p-2 prose-th:text-left prose-thead:bg-stone-800 prose-td:p-2 even:prose-tr:bg-stone-500/10 odd:prose-tr:bg-stone-700/10 prose-table:border prose-table:border-stone-700 px-6 md:px-10 lg:px-16 prose-headings:text-stone-200">
+        class="md px-6 md:px-10 lg:px-16">
         <slot />
       </main>
       {#if !dev}
