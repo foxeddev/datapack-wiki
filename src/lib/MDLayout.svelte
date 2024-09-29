69b5ddb4d3ca2789405aa636a40b806afb3a3fdd
Silabear <56885288+Silabear@users.noreply.github.com>
2024-09-29 19:23:12 +0100

    custom item guides, tabs mockup, guide seo changes, etc

--- src/lib/MDLayout.svelte
+++ src/lib/MDLayout.svelte
@@ -3,11 +3,14 @@
 
   export let title;
   export let description;
+  export let tags = "";
+
+  let tags_arr = tags.split(",").map(el => el.trim()).filter(String)
 </script>
 
 <Seo
   title="{title} | Datapack Wiki"
-  {description}
+  description={description}
   author="Datapack Wiki"
   siteName="Datapack Wiki"
   keywords="datapacks, datapack wiki, datapack docs, minecraft datapacks, datapack help, datapack creation, dph"
@@ -21,4 +24,12 @@
 
 <main class="md px-6 md:px-10 lg:px-16 prose-headings:text-stone-200">
   <slot />
+  {#if tags}
+  <div class="bg-stone-950/40 p-2 rounded-lg flex items-center space-x-3 my-10">
+    <span class="uppercase text-sm text-zinc-500">Tags:</span>
+    {#each tags_arr ?? [] as tag}
+      <span class="border border-yellow-500 px-1 text-yellow-500 rounded-lg uppercase text-sm">{tag}</span>
+    {/each}
+  </div>
+  {/if}
 </main>
---
+++ src/lib/reusables/Tabs.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import IconTick from "~icons/tabler/rosette-discount-check";
+</script>
+
+<div class="bg-stone-500/5 w-full flex items-center border-stone-700 border-t border-l border-r">
+  <span class="bg-stone-700 p-3 py-2 uppercase">
+    post 1.20.5
+  </span>
+  <span class="p-3 py-2 uppercase">
+    pre 1.20.5
+  </span>
+</div>
+<div class="bg-stone-500/5 w-full mb-4 flex items-center p-3 py-2 border-stone-700 border">
+  Text text text text text
+</div>
