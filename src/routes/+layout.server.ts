5a6466a37231c46c0178bb870d93944f9156e73b
Cobblestone <aidenhperry@gmail.com>
2024-11-09 17:37:00 -0500

    i will eventually learn that -a does not actually add the files

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,19 @@
+import type { LayoutServerLoad } from "./$types";
+
+type PackFormatEntry = {
+  data_pack_version: number;
+  stable: boolean;
+  id: string;
+};
+
+export const load = (async () => {
+  const url = "https://raw.githubusercontent.com/misode/mcmeta/summary/versions/data.min.json";
+
+  const data: PackFormatEntry[] = await fetch(url).then(r => r.json());
+  const filtered = data.filter(v => v.stable);
+
+  return {
+    packFormat: filtered[0].data_pack_version,
+    gameVersion: filtered[0].id
+  };
+}) satisfies LayoutServerLoad;
