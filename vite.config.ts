faffddbde2a6d0b89bd727de72abc3bb8d35e9fb
Cobblestone <aidenhperry@gmail.com>
2024-11-27 16:29:34 -0500

    huge dependency updates n stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { browserslistToTargets } from "lightningcss";
+import browserslist from "browserslist";
 
 export default defineConfig({
   plugins: [
@@ -12,4 +14,10 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  build: {
+    cssMinify: "lightningcss",
+  },
+  css: {
+    transformer: "lightningcss"
+  }
 });
