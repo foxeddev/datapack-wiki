f7d42bb45cb12c1dfd88722878e9549aec302067
Cobblestone <aidenhperry@gmail.com>
2024-11-27 21:52:15 -0500

    npm does NOT like mismatched dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { browserslistToTargets } from "lightningcss";
-import browserslist from "browserslist";
 
 export default defineConfig({
   plugins: [
