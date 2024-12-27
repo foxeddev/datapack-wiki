364a255320010ea1801ecff25b246eab67c43ba0
Silabear <56885288+Silabear@users.noreply.github.com>
2024-12-27 16:18:35 +0000

    add scoreboard page (and rework sidebar accordingly) + merge nbt pages

--- src/routes/wiki/nbt/snbt/+server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import type { RequestHandler } from "./$types";
-
-export const prerender = true;
-
-export const GET: RequestHandler = () => {
-  redirect(308, "/wiki/nbt/nbt");
-};
