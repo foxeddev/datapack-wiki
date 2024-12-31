ab734e75510c19dd7ef7a7cd3e15adf6a9a2d9cd
Cobblestone <aidenhperry@gmail.com>
2024-12-31 12:37:27 -0500

    default changefreq is now weekly

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -6,6 +6,7 @@ export const prerender = true;
 export const GET: RequestHandler = async () => {
   return await sitemap.response({
     origin: "https://datapack.wiki",
-    excludeRoutePatterns: ["^/sitemap.xml", "^/meta.json"],
+    excludeRoutePatterns: ["^/sitemap.xml", "^/meta.json", "^/robots.txt", "^/search.json"],
+    defaultChangefreq: "weekly"
   });
 };
