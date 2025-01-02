0035308d43a67f5c453a3f7bcc6041c6e8729aa1
Cobblestone <aidenhperry@gmail.com>
2025-01-02 13:16:27 -0500

    minor updates and clean up

--- svelte.config.js
+++ svelte.config.js
@@ -36,12 +36,7 @@ const admonitionsOptions = {
       ifmClass: "info",
       keyword: "info",
       svg: '<svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 9h.01" /><path d="M11 12h1v4h1" /></svg>',
-    },
-    caution: {
-      ifmClass: "warning",
-      keyword: "caution",
-      svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"/></svg>',
-    },
+    }
   },
 };
 
